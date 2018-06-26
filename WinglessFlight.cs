353aeadb9669a4e8f072bde6c90c8064a816f9cd
Brian Dixon <brianide@gmail.com>
2018-06-26 18:50:01 -0500

    Added properties for Hamstar Helpers issue tracking

--- WinglessFlight.cs
+++ WinglessFlight.cs
@@ -4,5 +4,8 @@ namespace WinglessFlight
 {
     class WinglessFlight : Mod
     {
+        // Properties for Hamstar issue reporting
+        public static string GithubUserName { get { return "brianide"; } }
+        public static string GithubProjectName { get { return "WinglessFlight"; } }
     }
 }
